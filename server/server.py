7cbcb9e921711597a8eacc45904cb8ca22bf4541
Andrew Wong <andrew.j.wong@outlook.com>
2019-03-15 22:59:49 +1100

    Add SQL table schema

--- server/server.py
+++ server/server.py
@@ -4,4 +4,10 @@ app = Flask(__name__, template_folder = '../site/templates', static_folder = '..
 import routes
 for module in routes.__modules: app.register_blueprint(routes.__modules[module].site)
 
-app.run("0.0.0.0", 1313, debug=True)
+from lib import database
+from lib.tableQueries import SQL
+
+for value in SQL.values():
+    database.create_table(value)
+
+app.run("0.0.0.0", 1313, debug=False)
