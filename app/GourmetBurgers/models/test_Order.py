2aa6f39c5f16b67991c8d0ea09538b91a42ba08f
Tong Zheng <z5142003@student.unsw.edu.au>
2019-04-04 17:28:25 +1100

    added historical testing

--- app/GourmetBurgers/models/test_Order.py
+++ app/GourmetBurgers/models/test_Order.py
@@ -10,6 +10,7 @@ import sqlite3
 def sys():
     with sqlite3.connect("../test_db.sqlite3") as _db:
         queries = list(_db.iterdump())
+        [print(line) for line in queries]
     sys = GBSystem(':memory:')
     for line in queries:
         try:
@@ -46,12 +47,34 @@ def test_orders_items(sys):
 	assert len(a.items) == 2
 	assert len(b.items) == 1
 
+
+	print([x.name for x in a.items])
+	MenuItem_a1 = a.items[0]
+
+	# print("AAAAA",MenuItem_a1.id)
+	assert MenuItem_a1.price == 8
+	assert MenuItem_a1.quantity == 1
+	assert MenuItem_a1.is_custom ==False
+
+	MenuItem_a2 = a.items[1]
+
+	assert MenuItem_a2.price == 8
+	assert MenuItem_a2.quantity == 1
+	assert MenuItem_a2.is_custom == True
+
+
 def test_complete_order(sys):
 	a = Order(1)
 	assert a.status == False
 
 	a.completeOrder()
 	assert a.status == True
+
+def test_no_order(sys):
+	with pytest.raises(NoItemError):
+		a = Order(3)
+
+
 
 
 
