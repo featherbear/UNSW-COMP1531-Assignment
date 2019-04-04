b8f4b09036da817487a2843022d50ce626a4b97b
Tong Zheng <z5142003@student.unsw.edu.au>
2019-04-04 13:48:36 +1100

    added two tests

--- app/GourmetBurgers/models/test_MenuItem.py
+++ app/GourmetBurgers/models/test_MenuItem.py
@@ -32,20 +32,39 @@ def sys():
 # with pytest.raises(Exceptions.NoItemError):
 #     sys.getMenuItem(None)
 
-def test_available(sys)
+def test_item(sys):
 
 	a = MenuItem(2)
+	b = MenuItem(3)
 	# a._components[1] = 3
 	# a._components[2] = 3
 
 	# b = MenuItem(3,3,2,5)
+	assert a._name == "MenuTwo"
+	assert b._name == "MenuThree"
 
-	assert a.id == 2
-	assert b.id == 3
+	assert a._description == "MenuTwo"
+	assert b._description == "MenuThree"
 
-	assert a.available == True
+	assert a._price == 2
+	assert b._price == 3
 
-def unavailable_item(sys)
+	assert a._id == 2
+	assert b._id == 3
+
+
+def test_usage(sys):
+
+
+def test_available(sys):
+	item_a = MenuItem(1)
+	item_b = MenuItem(2)
+
+	assert item_a.available == True
+	assert item_b.available == False
+
+
+def unavailable_item(sys):
 	c = MenuItem(6)
 	with pytest.raises(Exceptions.NoItemError):
 
