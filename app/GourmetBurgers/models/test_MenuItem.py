2e6393f4240826c28e4bd679dbbc6d07005cb094
Tong Zheng <z5142003@student.unsw.edu.au>
2019-04-04 15:20:06 +1100

    added some testings of menuItem

--- app/GourmetBurgers/models/test_MenuItem.py
+++ app/GourmetBurgers/models/test_MenuItem.py
@@ -33,42 +33,56 @@ def sys():
 #     sys.getMenuItem(None)
 
 def test_item(sys):
-
 	a = MenuItem(2)
 	b = MenuItem(3)
+	c = MenuItem(4)
 	# a._components[1] = 3
 	# a._components[2] = 3
-
 	# b = MenuItem(3,3,2,5)
 	assert a._name == "MenuTwo"
 	assert b._name == "MenuThree"
-
+	assert c._name == "MenuFour"
 	assert a._description == "MenuTwo"
 	assert b._description == "MenuThree"
-
+	assert c._description == "MenuFour"
 	assert a._price == 2
 	assert b._price == 3
-
 	assert a._id == 2
 	assert b._id == 3
 
 
-def test_usage(sys):
+def test_can_custom(sys):
+	item_a = MenuItem(1)
+	item_b = MenuItem(2)
+	item_c = MenuItem(5)
+
+	assert item_a.can_custome == True
+	assert item_b.can_custome == False
+	assert item_c.can_custome == True
+
+# def test_component_usage(sys):
+# 	item_a = MenuItem(1)
+# 	item_b = MenuItem(2)
+# 	item_c = MenuItem(5)
 
 
 def test_available(sys):
 	item_a = MenuItem(1)
 	item_b = MenuItem(2)
+	item_c = MenuItem(3)
+	item_d = MenuItem(4)
+	item_e = MenuItem(5)
 
 	assert item_a.available == True
 	assert item_b.available == False
+	assert item_c.available == True
+	assert item_d.available == True
+	assert item_e.available == True
 
 
-def unavailable_item(sys):
-	c = MenuItem(6)
+def item_not_found_error(sys):
 	with pytest.raises(Exceptions.NoItemError):
+		c = MenuItem(6)
 
-
-
-def test_usage()
+# def test_usage()
 
