6557a8eb14af1aa37ce52ba924b0563c074bb29a
Tong Zheng <z5142003@student.unsw.edu.au>
2019-04-04 17:27:37 +1100

    added component testing

--- app/GourmetBurgers/models/test_MenuItem.py
+++ app/GourmetBurgers/models/test_MenuItem.py
@@ -41,23 +41,23 @@ def test_item(sys):
 	# a._components[1] = 3
 	# a._components[2] = 3
 	# b = MenuItem(3,3,2,5)
-	assert a._name == "MenuTwo"
-	assert b._name == "MenuThree"
-	assert c._name == "MenuFour"
-	assert a._description == "MenuTwo"
-	assert b._description == "MenuThree"
-	assert c._description == "MenuFour"
+	assert a.name == "MenuTwo"
+	assert b.name == "MenuThree"
+	assert c.name == "MenuFour"
+	assert a.description == "MenuTwo"
+	assert b.description == "MenuThree"
+	assert c.description == "MenuFour"
 
 def test_attributes(sys):
 	a = MenuItem(2)
 	b = MenuItem(3)
 	c = MenuItem(4)
-	assert a._price == 2
-	assert b._price == 3
-	assert c._price == 4
-	assert a._id == 2
-	assert b._id == 3
-	assert c._id == 4
+	assert a.price == 2
+	assert b.price == 3
+	assert c.price == 4
+	assert a.id == 2
+	assert b.id == 3
+	assert c.id == 4
 
 
 def test_can_custom(sys):
@@ -69,10 +69,12 @@ def test_can_custom(sys):
 	assert item_b.can_customise == False
 	assert item_c.can_customise == True
 
-# def test_component_usage(sys):
-# 	item_a = MenuItem(1)
-# 	item_b = MenuItem(2)
-# 	item_c = MenuItem(5)
+def test_components(sys):
+	item_a = MenuItem(1)
+	for component in item_a.components:
+		assert type(component) is MenuIngredient
+
+	assert item_a.components[0].id == 1
 
 
 def test_available(sys):
