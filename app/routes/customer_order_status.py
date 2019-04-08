0da3dd26aefa6e06bbf53080d342d6cde7ef0386
Weiting <z5210581@ad.unsw.edu.au>
2019-04-08 13:25:33 +1000

    get information of order

--- app/routes/customer_order_status.py
+++ app/routes/customer_order_status.py
@@ -1,4 +1,4 @@
-from System i
+from System import GBSystem
 from flask import Blueprint, render_template, request, g, current_app as app
 from lib import util
 
@@ -6,4 +6,8 @@ site = Blueprint(__name__, __name__)
 
 @site.route('/customer_order_status')
 def customer_order_status(orderID):
-
+    order = GBSystem.getOrder(orderID)
+    datetime = order.datetime
+    status = order.status
+    price = order.price
+    return render_template("customer_order_status.html",orderID = orderID, datetime = datetime, status = status, price = price)
