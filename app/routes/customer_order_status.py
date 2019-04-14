89c3e8d5fd096e3b43b85c69a02b9346de39e920
Weiting <z5210581@ad.unsw.edu.au>
2019-04-14 20:14:26 +1000

    now display items in the order

--- app/routes/customer_order_status.py
+++ app/routes/customer_order_status.py
@@ -1,13 +1,15 @@
-from GourmetBurgers import GBSystem
+from GourmetBurgers import models
 from flask import Blueprint, render_template, request, g, current_app as app
 from lib import util
 
 site = Blueprint(__name__, __name__)
 
-@site.route('/customer_order_status')
+@site.route('/status/<orderID>')
 def customer_order_status(orderID):
-    order = GBSystem().getOrder(orderID)
+    order = models.Order(orderID)
     date = order.date
     status = order.status
     price = order.price
-    return render_template("customer_order_status.html",orderID = orderID, date = date, status = status, price = price)
+    items = order.items
+    return render_template("customer_order_status.html",orderID = orderID, date = date, status = status, price = price, items = items)
+   
