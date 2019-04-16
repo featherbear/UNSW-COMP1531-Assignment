cc1b2b83db61429a5173aa72c2f24ec8db689a85
Tong Zheng <z5142003@student.unsw.edu.au>
2019-04-16 16:11:25 +1000

    added a new route for update stock

--- app/routes/staff_inventory.py
+++ app/routes/staff_inventory.py
@@ -6,5 +6,13 @@ site = Blueprint(__name__, __name__)
 
 @site.route('/staff/inventory/')
 def view_inventory():
-	inventory = app.GB.getInventory
+	inventory = app.GB.getInventoryMap()
     return render_template('staff_inventory.html', inventory = inventory)
+
+
+@site.route('/staff/inventory/update',methods = ['POST'])
+def update_inventory():
+	ingredient = app.GB.getIngredient(request.form['id']
+	ingredient.updateStock(request.form['new_stock'])
+
+	return redirect("/staff/inventory/")
