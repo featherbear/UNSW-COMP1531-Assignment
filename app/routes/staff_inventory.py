f257874e74637d54aabeec6ee1df6673e42479dd
Tong Zheng <z5142003@student.unsw.edu.au>
2019-04-18 15:47:06 +1000

    finished inventory dashboard

--- app/routes/staff_inventory.py
+++ app/routes/staff_inventory.py
@@ -6,13 +6,12 @@ site = Blueprint(__name__, __name__)
 
 @site.route('/staff/inventory/')
 def view_inventory():
-	inventory = app.GB.getInventoryMap()
-    return render_template('staff_inventory.html', inventory = inventory)
+    return render_template('staff_inventory.html', inventory = app.GB.getInventoryMap())
 
 
 @site.route('/staff/inventory/update', methods=['POST'])
 def update_inventory():
 	ingredient = app.GB.getIngredient(request.form['id'])
-	ingredient.updateStock(request.form['new_stock'])
+	ingredient.updateStock(int(request.form['new_stock']))
 
 	return redirect("/staff/inventory/")
