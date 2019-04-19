db47d693660663aff9d47f19e99c170cb4203164
Weiting <z5210581@ad.unsw.edu.au>
2019-04-19 18:58:11 +1000

    add routes of customise

--- app/routes/menu.py
+++ app/routes/menu.py
@@ -1,3 +1,4 @@
+from GourmetBurgers import models
 from flask import Blueprint, render_template
 
 site = Blueprint(__name__, __name__)
@@ -5,3 +6,9 @@ site = Blueprint(__name__, __name__)
 @site.route('/menu')
 def viewMenu():
    return render_template('menu.html')
+
+@site.route('/menu/customise/<menuID>')
+def customise(menuID):
+   menuItem = models.MenuItem(menuID)
+   components = menuItem.components
+   return render_template('customise.html', can_customise = menuItem.can_customise, components = components)
