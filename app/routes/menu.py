8db4153d07b281d49c5dcb3ffd1bf802c984eec9
Weiting <z5210581@ad.unsw.edu.au>
2019-04-19 21:53:57 +1000

    need to modify method of updating quantity

--- app/routes/menu.py
+++ app/routes/menu.py
@@ -1,5 +1,5 @@
 from GourmetBurgers import models
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 site = Blueprint(__name__, __name__)
 
@@ -7,8 +7,22 @@ site = Blueprint(__name__, __name__)
 def viewMenu():
    return render_template('menu.html')
 
-@site.route('/menu/customise/<menuID>')
+@site.route('/menu/customise/<menuID>', methods=["GET","POST"])
 def customise(menuID):
    menuItem = models.MenuItem(menuID)
+   if not menuItem.can_customise:
+      return render_template('customise.html', can_customise = menuItem.can_customise)
+  
    components = menuItem.components
+   if request == "POST":
+      for item in components:
+         decrease = 'decrease' + f'{item.id}'
+         increase = 'increase' + f'{item.id}'
+         if decrease in request.form:
+            #update quantity
+            pass
+         elif increase in request.form:
+            #update quantity
+            pass
+
    return render_template('customise.html', can_customise = menuItem.can_customise, components = components)
