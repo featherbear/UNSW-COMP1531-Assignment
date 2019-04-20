8c3e7c6f15de8dc383acebc49c6d58d65ce5a00d
Weiting <z5210581@ad.unsw.edu.au>
2019-04-20 17:15:54 +1000

    add method to update quantity

--- app/routes/menu.py
+++ app/routes/menu.py
@@ -10,19 +10,25 @@ def viewMenu():
 @site.route('/menu/customise/<menuID>', methods=["GET","POST"])
 def customise(menuID):
    menuItem = models.MenuItem(menuID)
+   print("hello")
    if not menuItem.can_customise:
       return render_template('customise.html', can_customise = menuItem.can_customise)
   
    components = menuItem.components
-   if request == "POST":
+   if request.method == "POST":
+      print("hello")
+      quantities = {}
       for item in components:
-         decrease = 'decrease' + f'{item.id}'
-         increase = 'increase' + f'{item.id}'
+         quantities[item.id] = request.form[f'quantity{item.id}']
+         decrease = f'decrease{item.id}'
+         increase = f'increase{item.id}'
          if decrease in request.form:
             #update quantity
-            pass
+            quantities[item.id] = str(int(quantities[item.id]) - 1)
          elif increase in request.form:
             #update quantity
-            pass
+            quantities[item.id] = str(int(quantities[item.id]) + 1)
+      
+      return render_template('customise.html', can_customise = menuItem.can_customise, components = components, quantities = quantities)
 
    return render_template('customise.html', can_customise = menuItem.can_customise, components = components)
